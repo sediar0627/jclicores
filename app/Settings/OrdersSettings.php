c1dc44e262bdbc3ad34d4d2f17bba3ee060f9964
Sebastian Diaz <sdiazarrieta0627@gmail.com>
2024-04-27 13:13:43 -0500

    primer despliegue

---
+++ app/Settings/OrdersSettings.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Settings;
+
+use App\Classes\SettingForm;
+use App\Services\Options;
+use App\Services\SettingsPage;
+
+class OrdersSettings extends SettingsPage
+{
+    const IDENTIFIER = 'orders';
+
+    const AUTOLOAD = true;
+
+    public function __construct()
+    {
+        $options = app()->make( Options::class );
+
+        $this->form = SettingForm::form(
+            title: __( 'Orders Settings' ),
+            description: __( 'configure settings that applies to orders.' ),
+            tabs: include ( dirname( __FILE__ ) . '/orders/general.php' )
+        );
+
+        // $this->form = [
+        //     'title' => __( 'Orders Settings' ),
+        //     'description' => __( 'configure settings that applies to orders.' ),
+        //     'tabs' => [
+        //         'layout' => include ( dirname( __FILE__ ) . '/orders/general.php' ),
+        //     ],
+        // ];
+    }
+}
